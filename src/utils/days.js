d59300f2b5db6bd58b65bb9611c95f85c801d323
Daniel Barria <daniel.barria@cmpc.cl>
2023-04-21 16:58:51 -0400

    warnings

--- src/utils/days.js
+++ src/utils/days.js
@@ -8,11 +8,11 @@ exports.daysBetween = function( date1, date2 ) {
     let difference_ms = date2_ms - date1_ms;
     //take out milliseconds
     difference_ms = difference_ms/1000;
-    const seconds = Math.floor(difference_ms % 60);
+    //const seconds = Math.floor(difference_ms % 60);
     difference_ms = difference_ms/60; 
-    const minutes = Math.floor(difference_ms % 60);
+    //const minutes = Math.floor(difference_ms % 60);
     difference_ms = difference_ms/60; 
-    const hours = Math.floor(difference_ms % 24);  
+    //const hours = Math.floor(difference_ms % 24);  
     const days = Math.floor(difference_ms/24);
     
     return days;
